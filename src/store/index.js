524b16fde9a5ee7c6719183dcea94c5f755a215e
ShaqeTarverdyan <tshaqe@gmail.com>
2020-11-13 18:23:10 +0400

    initiate redux-saga  on rootSaga file

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,19 @@
 import { compose, createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
 import rootReducer from "./reducers/rootReducer";
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas'
 
+
+const sagaMiddleware = createSagaMiddleware()
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     rootReducer,
+    {},
     composeEnhancers(
-        applyMiddleware(thunk)
+        applyMiddleware(sagaMiddleware)
     )
 );
+
+sagaMiddleware.run(rootSaga)
 
 export default store;
