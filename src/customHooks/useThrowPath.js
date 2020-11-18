3eb77bc713817a8528ce03c8ae898ce6a75ac607
ShaqeTarverdyan <tshaqe@gmail.com>
2020-11-18 16:47:00 +0400

    remove not needed file

--- src/customHooks/useThrowPath.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-
-
-export const useThrowPath = (path) => {
-    let history = useHistory();
-    return history.push(path);
-}
